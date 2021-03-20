d52c09ed6df9895858107568d7a08a7cde3bd764
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-03-20 19:19:21 +0100

    Finish task1.1

--- snn.py
+++ snn.py
@@ -1,12 +1,39 @@
 """Task 1.1 STL-10 classification using simple neural network"""
 
 import tensorflow as tf
+import tensorflow.keras as ka
 import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
 
-ds = tfds.load(
+train, test = tfds.load(
     "stl10",
-    split="train",
+    split=["train", "test"],
     as_supervised=True,
-    download=True,
+    shuffle_files=False,
+    batch_size=64,
+    download=False,
     data_dir="./datasets/",
 )
+
+model = ka.Sequential(
+    [
+        ka.layers.experimental.preprocessing.Rescaling(1.0 / 255.0),
+        ka.layers.experimental.preprocessing.Normalization(mean=0.45, variance=0.8),
+        ka.layers.Flatten(input_shape=(96, 96, 3)),
+        ka.layers.Dense(94, activation="tanh"),
+        ka.layers.Dense(10, activation="sigmoid"),
+        ka.layers.Softmax(),
+    ]
+)
+
+model.compile(
+    optimizer=ka.optimizers.Adam(),
+    loss=ka.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+model.fit(train, epochs=10)
+
+test_loss, test_acc = model.evaluate(test, verbose=2)
+
+print(f"Test loss: {test_loss}, Test accuracy: {test_acc}")
