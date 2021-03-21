1449032c6c1833a0c91f35288311c020c9da019d
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-03-21 20:01:21 +0100

    Add some comments

--- snn.py
+++ snn.py
@@ -1,10 +1,11 @@
 """Task 1.1 STL-10 classification using simple neural network"""
 
 import tensorflow as tf
-import tensorflow.keras as ka
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
+from tensorflow import keras
 
+# Load the dataset split into training data (images and labels), and testing data.
 train, test = tfds.load(
     "stl10",
     split=["train", "test"],
@@ -15,25 +16,28 @@ train, test = tfds.load(
     data_dir="./datasets/",
 )
 
-model = ka.Sequential(
+# Define the model
+model = keras.Sequential(
     [
-        ka.layers.experimental.preprocessing.Rescaling(1.0 / 255.0),
-        ka.layers.experimental.preprocessing.Normalization(mean=0.45, variance=0.8),
-        ka.layers.Flatten(input_shape=(96, 96, 3)),
-        ka.layers.Dense(94, activation="tanh"),
-        ka.layers.Dense(10, activation="sigmoid"),
-        ka.layers.Softmax(),
+        keras.layers.experimental.preprocessing.Rescaling(1.0 / 255.0),
+        keras.layers.experimental.preprocessing.Normalization(mean=0.45, variance=0.8),
+        keras.layers.Flatten(input_shape=(96, 96, 3)),
+        keras.layers.Dense(94, activation="tanh"),
+        keras.layers.Dense(10, activation="sigmoid"),
+        keras.layers.Softmax(),
     ]
 )
 
+# Compile the model
 model.compile(
-    optimizer=ka.optimizers.Adam(),
-    loss=ka.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.Adam(),
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=["accuracy"],
 )
 
+# Fit the model to the training data
 model.fit(train, epochs=10)
 
+# Evaluate the performence of the model, based on loss and accuracy
 test_loss, test_acc = model.evaluate(test, verbose=2)
-
 print(f"Test loss: {test_loss}, Test accuracy: {test_acc}")
